90374b20574e6be73971990c6211e411861176b3
rascal46 <sutirtho17@gmail.com>
2025-01-03 13:33:02 +0530

    Add files via upload

---
+++ Simple Python Calculator.py
@@ -0,0 +1,51 @@
+m=input()
+
+e=input()
+
+n=input()
+
+m=int(m)
+n=int(n)
+
+if (e == '+') :
+
+	a = m+n
+
+	print("Answer:\n")
+	print(a)
+
+	
+
+elif (e == '-') : 
+
+	b = m-n
+
+	print("Answer:\n")
+	print(b)
+
+	
+
+elif (e == '*') :
+
+	c = m*n
+
+	print("Answer:\n")
+	print(c)
+
+	
+
+elif (e == '/') : 
+
+    if (n != 0) :
+
+        d = m/n
+
+        print("Answer:\n")
+        print(d)
+
+    else :
+
+        print ("NOT VALID!")
+
+			
+
